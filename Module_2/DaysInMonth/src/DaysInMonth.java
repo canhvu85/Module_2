e5e7743c42ae616119249304b01c2a2d3f4ce500
anhvu <canhvu85@gmail.com>
2019-10-11 10:55:32 +0700

    add DaysInMonth

---
+++ Module_2/DaysInMonth/src/DaysInMonth.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class DaysInMonth {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Input month");
+        int month = scanner.nextInt();
+
+        String daysInMonth ;
+
+        switch (month){
+            case 2 :
+                daysInMonth ="28 or 29";
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                daysInMonth = "31";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                daysInMonth = "30";
+                break;
+            default:
+                daysInMonth = "";
+        }
+
+        System.out.println(daysInMonth);
+
+    }
+}
