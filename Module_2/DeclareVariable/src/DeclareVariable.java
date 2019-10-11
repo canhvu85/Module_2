af38f52a01a8bf45205826f1078622b001cab799
anhvu <canhvu85@gmail.com>
2019-10-11 08:39:41 +0700

    add DeclareVariable

---
+++ Module_2/DeclareVariable/src/DeclareVariable.java
@@ -0,0 +1,17 @@
+public class DeclareVariable {
+    public static void main(String[] args){
+        int i = 10;
+        float f = 20.5f;
+        double d = 20.5;
+        boolean b = true;
+        char c = 'a';
+        String s = "ha noi";
+
+        System.out.println("i = " + i);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
