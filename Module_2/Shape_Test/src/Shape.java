53f4ba994c0e806a36de0cd05bc944b83f2cf10c
anhvu <canhvu85@gmail.com>
2019-10-17 14:37:43 +0700

    update ShapeTest

--- Module_2/Shape_Test/src/Shape.java
+++ Module_2/Shape_Test/src/Shape.java
@@ -1,16 +1,55 @@
 public class Shape {
-    public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(3,5);
-        Circle circle = new Circle(3);
-        Square square = new Square(5);
+    private Rectangle rectangle;
+    private Square square;
+    private Circle circle;
 
-        rectangle.draw();
-        System.out.println(rectangle.area());
+    public Shape(Rectangle rectangle, Square square, Circle circle) {
+        this.rectangle = rectangle;
+        this.square = square;
+        this.circle = circle;
+    }
 
-        circle.draw();
-        System.out.println(circle.area());
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
 
+    public void setRectangle(Rectangle rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public Square getSquare() {
+        return square;
+    }
+
+    public void setSquare(Square square) {
+        this.square = square;
+    }
+
+    public Circle getCircle() {
+        return circle;
+    }
+
+    public void setCircle(Circle circle) {
+        this.circle = circle;
+    }
+
+    public void draw(){
         square.draw();
-        System.out.println(square.area());
+        rectangle.draw();
+        circle.draw();
+    }
+
+    public double area(){
+        return square.area()+rectangle.area()+circle.area();
+    }
+
+    public static void main(String[] args) {
+//       Rectangle rectangle = new Rectangle(3,5);
+//       Square square = new Square(6);
+//       Circle circle = new Circle(8);
+       Shape shape = new Shape(new Rectangle(3,5),new Square(3),new Circle(2));
+
+       shape.draw();
+       System.out.println(shape.getRectangle().area());
     }
 }
